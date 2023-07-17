adeedebc38f51155d9cbefc743dc09833cc67403
abdulrahman <aboood1999@live.com>
2023-07-17 14:06:16 +0300

    minor changes to fix bugs appear on console

--- src/Data/MostPopulerItemsData.js
+++ src/Data/MostPopulerItemsData.js
@@ -10,13 +10,13 @@ import popular_08 from '../assets/images/popular-08.jpg';
 
 const MostPopularItemsData = [
     {id: "most_populer_item_0" ,title: 'Fortnite'  ,category: 'Battle royale'  ,rating: '3.8'  ,download: '76.6M'  ,image: popular_01 }
-   ,{id: "most_populer_item_0" ,title: 'Dota 2'  ,category: 'MOBA'  ,rating: '3.8'  ,download: '20.1M'  ,image: popular_02 }
-   ,{id: "most_populer_item_0" ,title: 'MineCrafe'  ,category: 'Survivel'  ,rating: '3.8'  ,download: '100.3M'  ,image: popular_03 }
-   ,{id: "most_populer_item_0" ,title: 'TFT'  ,category: 'Stratgy'  ,rating: '3.8'  ,download: '12.8M'  ,image: popular_04 }
-   ,{id: "most_populer_item_0" ,title: 'LOL'  ,category: 'MOBA'  ,rating: '3.8'  ,download: '55.3M'  ,image: popular_05 }
-   ,{id: "most_populer_item_0" ,title: 'Valorant'  ,category: 'Shooting'  ,rating: '3.8'  ,download: '10.8M'  ,image: popular_06 }
-   ,{id: "most_populer_item_0" ,title: 'OverWatch'  ,category: 'Hero shooter'  ,rating: '3.8'  ,download: '7.2M'  ,image: popular_07 }
-   ,{id: "most_populer_item_0" ,title: 'LoopHero'  ,category: 'RPG'  ,rating: '3.8'  ,download: '900.5K'  ,image: popular_08 }  
+   ,{id: "most_populer_item_1" ,title: 'Dota 2'  ,category: 'MOBA'  ,rating: '3.8'  ,download: '20.1M'  ,image: popular_02 }
+   ,{id: "most_populer_item_2" ,title: 'MineCrafe'  ,category: 'Survivel'  ,rating: '3.8'  ,download: '100.3M'  ,image: popular_03 }
+   ,{id: "most_populer_item_3" ,title: 'TFT'  ,category: 'Stratgy'  ,rating: '3.8'  ,download: '12.8M'  ,image: popular_04 }
+   ,{id: "most_populer_item_4" ,title: 'LOL'  ,category: 'MOBA'  ,rating: '3.8'  ,download: '55.3M'  ,image: popular_05 }
+   ,{id: "most_populer_item_5" ,title: 'Valorant'  ,category: 'Shooting'  ,rating: '3.8'  ,download: '10.8M'  ,image: popular_06 }
+   ,{id: "most_populer_item_6" ,title: 'OverWatch'  ,category: 'Hero shooter'  ,rating: '3.8'  ,download: '7.2M'  ,image: popular_07 }
+   ,{id: "most_populer_item_7" ,title: 'LoopHero'  ,category: 'RPG'  ,rating: '3.8'  ,download: '900.5K'  ,image: popular_08 }  
 ]
 
 
